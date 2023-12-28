375eabf1d701d36ab36b52c339b85c057a6d6d09
billhafidz <billhafidz14@gmail.com>
2023-12-28 17:40:54 +0700

    fitur member dan komik

--- UAS_OOP/src/main/java/org/itenas/uas/serviceimpl/MemberServiceImpl.java
+++ UAS_OOP/src/main/java/org/itenas/uas/serviceimpl/MemberServiceImpl.java
@@ -4,6 +4,7 @@
  */
 package org.itenas.uas.serviceimpl;
 
+import org.itenas.uas.pojo.Akun;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -18,7 +19,7 @@ import org.itenas.uas.utilities.ConnectionManager;
 
 /**
  *
- * @author Win10
+ * @author Billhafidz
  */
 public class MemberServiceImpl implements MemberService{
     private ConnectionManager conMan;
@@ -26,6 +27,45 @@ public class MemberServiceImpl implements MemberService{
     Statement stmt;
     ResultSet rs;
 
+    public Member login(String username, String password) {
+        Member member = null;
+        Akun akun = null;
+        String sql = "SELECT m.id_member, m.nama_member, m.alamat, m.email, m.nomor_telepon, "
+                + "ak.id_akun, ak.email, ak.username, ak.role "
+                + "FROM member m, akun ak "
+                + "WHERE m.id_akun = m.id_akun "
+                + "AND ak.username = '"+username+"' "
+                + "AND ak.password = '"+password+"'";
+        
+        conMan = new ConnectionManager();
+        conn = conMan.connect();
+        
+        try {
+            stmt = conn.createStatement();
+            rs = stmt.executeQuery(sql);
+            
+            while (rs.next()) {
+                member = new Member();
+                member.setId(rs.getString("id_member"));
+                member.setNama(rs.getString("nama_member"));
+                member.setAlamat(rs.getString("alamat"));
+                member.setEmail(rs.getString("email"));
+                member.setNomorTelp(rs.getString("nomor_telepon"));
+                akun = new Akun();
+                akun.setId(rs.getInt("id_akun"));
+                akun.setEmail(rs.getString("email"));
+                akun.setUsername(rs.getString("username"));
+                akun.setRole(rs.getString("role"));
+                member.setAkun(akun);
+            }
+            conMan.disconnect();
+        } catch (SQLException ex) {
+            Logger.getLogger(MemberServiceImpl.class.getName())
+                    .log(Level.SEVERE, null, ex);
+        }
+        return member;
+    }
+    
     @Override
     public List<Member> findAll() {
         List<Member> listMember = new ArrayList<>();
@@ -45,6 +85,12 @@ public class MemberServiceImpl implements MemberService{
                 member.setAlamat(rs.getString("alamat"));
                 member.setEmail(rs.getString("email"));
                 member.setNomorTelp(rs.getString("nomor_telepon"));
+                Akun akun = new Akun();
+                akun.setId(rs.getInt("id_akun"));
+                akun.setEmail(rs.getString("email"));
+                akun.setUsername(rs.getString("username"));
+                akun.setRole(rs.getString("role"));
+                member.setAkun(akun);
                 
                 listMember.add(member);
             }
@@ -65,7 +111,11 @@ public class MemberServiceImpl implements MemberService{
                 + ""+object.getNama()+", "
                 + "'"+object.getAlamat()+"', "
                 + "'"+object.getEmail()+"', "
-                + "'"+object.getNomorTelp()+"'"
+                + "'"+object.getNomorTelp()+"', "
+                + "'"+object.getAkun().getEmail()+"'"
+                + "'"+object.getAkun().getUsername()+"'"
+                + "'"+object.getAkun().getPassword()+"'"
+                + "'"+object.getAkun().getRole()+"'"
                 + "')";
         
         conMan = new ConnectionManager();
@@ -82,19 +132,84 @@ public class MemberServiceImpl implements MemberService{
         return result;
     }
 
-    @Override
+        @Override
     public String update(Member object) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        String result = "";
+        String sql = "UPDATE member SET id_member='"+object.getId()+"', "
+                + "nama_member="+object.getNama()+", "
+                + "alamat='"+object.getAlamat()+"', "
+                + "email='"+object.getEmail()+"', "
+                + "username='"+object.getAkun().getUsername()+"', "
+                + "password='"+object.getAkun().getPassword()+"' "
+                + "role='"+object.getAkun().getRole()+"' "
+                + "WHERE id_member="+object.getId()+"";
+        
+        conMan = new ConnectionManager();
+        conn = conMan.connect();
+        
+        try {
+            stmt = conn.createStatement();
+            stmt.executeUpdate(sql);
+            conMan.disconnect();
+        } catch (SQLException ex) {
+            Logger.getLogger(MemberServiceImpl.class.getName()).
+                    log(Level.SEVERE, null, ex);
+        }
+        return result;
     }
 
     @Override
     public Member findById(int id) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        Member member = null;
+        Akun akun = null;
+        String sql = "SELECT * FROM member WHERE id_member="+id+"";
+        
+        conMan = new ConnectionManager();
+        conn = conMan.connect();
+        
+        try {
+            stmt = conn.createStatement();
+            rs = stmt.executeQuery(sql);
+            
+            while (rs.next()) {
+                member = new Member();
+                member.setId(rs.getString("id_member"));
+                member.setNama(rs.getString("nama_member"));
+                member.setAlamat(rs.getString("alamat"));
+                member.setEmail(rs.getString("email"));
+                member.setNomorTelp(rs.getString("nomor_telepon"));
+                akun = new Akun();
+                akun.setId(rs.getInt("id_akun"));
+                akun.setEmail(rs.getString("email"));
+                akun.setUsername(rs.getString("username"));
+                akun.setRole(rs.getString("role"));
+                member.setAkun(akun);
+            }
+            conMan.disconnect();
+        } catch (SQLException ex) {
+            Logger.getLogger(MemberServiceImpl.class.getName())
+                    .log(Level.SEVERE, null, ex);
+        }
+        return member;
     }
 
     @Override
     public String delete(int id) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        String result = "";
+        String sql = "DELETE FROM member WHERE id_member="+id+"";
+        
+        conMan = new ConnectionManager();
+        conn = conMan.connect();
+        
+        try {
+            stmt = conn.createStatement();
+            stmt.executeUpdate(sql);
+            conMan.disconnect();
+        } catch (SQLException ex) {
+            Logger.getLogger(MemberServiceImpl.class.getName())
+                    .log(Level.SEVERE, null, ex);
+        }
+        return result;
     }
     
 }
